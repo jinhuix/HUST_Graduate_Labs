6552cecf6cba6fa22f8a8fc9ed22db4e459a3270
Imcaicai <2939361916@qq.com>
2024-12-29 10:51:20 +0000

    完成lab3-2

--- lab3/sim-usm2.py
+++ lab3/sim-usm2.py
@@ -22,24 +22,35 @@ if __name__ == "__main__":
     # STEP1 : Create USM device allocation for data1 and data2
     dev_q = SYCL_queue().init_queue_from_device(0, "gpu")
     dev_data1 = SYCL_memory()
+    dev_data1.malloc_shared(dev_q, N)
     dev_data2 = SYCL_memory()
+    dev_data2.malloc_shared(dev_q, N)
     
     # STEP2 : Copy data1 and data2 to USM device allocation
+    dev_data1.copy_from_host(data1)
+    dev_data2.copy_from_host(data2)
     
     # STEP3 : Write kernel code to update data1 on device with square of value, and Run the kernel function on device
     def kernel1(input):
-        pass
+        for i in range(len(input.mem)):
+            input.mem[i] = input.mem[i] * input.mem[i]
+    dev_q.parallel_for(kernel1, dev_data1).wait()  # 在设备上运行核函数
 
     # STEP4 : Write kernel code to update data2 on device with twice of value, and run the kernel function on device
     def kernel2(input):
-        pass
-    
+        for i in range(len(input.mem)):
+            input.mem[i] = input.mem[i] * 2
+    dev_q.parallel_for(kernel2, dev_data2).wait()  # 在设备上运行核函数
+
     # STEP5 : Write kernel code to add data2 on device to data1, and run the kernel function on device
     def kernel3(input1, input2):
-        pass
+        for i in range(len(input1.mem)):
+            input1.mem[i] = input1.mem[i] + input2.mem[i]
+    dev_q.parallel_for(kernel3, dev_data1, dev_data2).wait()  # 在设备上运行核函数
     
     # STEP6 : Copy data1 on device to host
     result = SYCL_memory().malloc(N)
+    result.copy_from_device(dev_data1)
 
     # ---------------------------- End ----------------------------
     # Check Correctness
